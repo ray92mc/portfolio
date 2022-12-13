9c047dff2b3ded3647f6f2f0518db74a70e7a948
Raymond McCarthy <x00179223@mytudublin.ie>
2022-12-13 14:15:18 +0000

    send email funtionality

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+function sendMail(){
+    var params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    };
+
+    const emailServiceId = "service_ajir7mo";
+    const templateId = "template_i7a7bp9";
+
+    emailjs.send(emailServiceId, templateId, params).then(res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message sent!");
+    }).catch(err=>console.log(err));
+}
+
+
